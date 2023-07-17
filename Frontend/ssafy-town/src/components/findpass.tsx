4a83884da5de0f90d775900fb8d4567bb6792d63
이진호 <blueland1001@naver.com>
2023-07-17 17:16:30 +0900

    Feat : findpass, mypage 추가

--- Frontend/ssafy-town/src/components/findpass.tsx
+++ Frontend/ssafy-town/src/components/findpass.tsx
@@ -31,17 +31,26 @@ const FindPassword: React.FC = () => {
   return (
     <div className="background">
       <div className="modal">
+        <div className="logo"/>
         <h1>비밀번호 찾기</h1>
-        <a className="link" href="/login">뒤로가기</a>
-        <img className="logo" alt="Logo" />
+        {/* <a className="link" href="/login">뒤로가기</a> */}
         <form onSubmit={formik.handleSubmit}>
           <input className="input" type="text" placeholder="성함" {...formik.getFieldProps('name')} />
-          {formik.touched.name && formik.errors.name ? <div>{formik.errors.name}</div> : null}
+          {/* {formik.touched.name && formik.errors.name ? <div>{formik.errors.name}</div> : null} */}
+          <div className="error">{formik.touched.name && formik.errors.name ? formik.errors.name : null}</div>
 
           <input className="input" type="text" placeholder="이메일" {...formik.getFieldProps('email')} />
-          {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
+          {/* {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null} */}
+          <div className="error">{formik.touched.email && formik.errors.email ? formik.errors.email : null}</div>
 
-          <button className="button" type="submit" disabled={!formik.isValid || formik.isSubmitting}>Find Password</button>
+          {/* <button className="button" type="submit" disabled={!formik.isValid || formik.isSubmitting}>Find Password</button> */}
+          <button className="button" id="findpass" type="submit" color="skyblue" disabled={!formik.isValid || formik.isSubmitting}>확인</button>
+          <button className="button" id="goback" type="button"  color="darkgrey" >
+            <a href="/login">뒤로가기</a>
+          </button>
+          <div className="button">
+            <a href="/Mypage">마이페이지 테스</a>
+          </div>
         </form>
       </div>
     </div>

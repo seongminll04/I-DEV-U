a5ad95613e2e9b157330212f4bcb58ecd47a5dab
bmsnc <qjatjs98@gmail.com>
2023-07-27 13:48:54 +0900

    Feat : Test 수정

--- Backend/mate/src/test/java/mate/user/UserTest.java
+++ Backend/mate/src/test/java/mate/user/UserTest.java
@@ -10,9 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.transaction.Transactional;
-import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 

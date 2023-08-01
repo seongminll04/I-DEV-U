b651c924e35a4464b5aa4cab7e1d5ee83c34c6ca
bmsnc <qjatjs98@gmail.com>
2023-08-02 00:24:23 +0900

    Feat : WebConfig 설정 완료

--- Backend/mate/src/main/java/mate/global/config/CorsConfig.java
+++ Backend/mate/src/main/java/mate/global/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("https://i9b206.p.ssafy.io:9090/"));
+        config.setAllowedOrigins(List.of("https://i9b206.p.ssafy.io/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Authorization-refresh", "*"));
         config.setExposedHeaders(List.of("Authorization", "Authorization-refresh", "*"));

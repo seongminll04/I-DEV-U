b6c3022575766b3bed724eab907332a74e770feb
bmsnc <qjatjs98@gmail.com>
2023-08-02 12:39:55 +0900

    Feat : localhost cors 수정C

--- Backend/mate/src/main/java/mate/global/jwt/service/RedisService.java
+++ Backend/mate/src/main/java/mate/global/jwt/service/RedisService.java
@@ -6,6 +6,8 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Duration;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -14,15 +16,16 @@ public class RedisService {
     private final RedisTemplate redisTemplate;
 
     @Value("${jwt.refresh.expiration}")
-    private Long refreshTokenExpirationPeriod;
+    private Long refreshExpirationPeriod;
 
     public String getData(String key) {
         return (String) redisTemplate.opsForValue().get(key);
     }
 
-    public void setDataWithExpiration(String key, String value, Long time) {
+    public void setDataWithExpiration(String key, String value) {
         if (this.getData(key) != null)
             this.deleteData(key);
+        Duration refreshTokenExpirationPeriod = Duration.ofSeconds(refreshExpirationPeriod);
         redisTemplate.opsForValue().set(key, value, refreshTokenExpirationPeriod);
     }
 

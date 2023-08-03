192ee272d023c52c1ef2d6a9249a483978905144
이진호 <blueland1001@naver.com>
2023-08-03 16:51:37 +0900

    Fix : videoController 주석처리 해제

--- Backend/mate/src/main/java/mate/controller/video/VideoController.java
+++ Backend/mate/src/main/java/mate/controller/video/VideoController.java
@@ -1,20 +1,23 @@
-//package mate.controller.video;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/video")
-//@CrossOrigin(origins = {})
-//public class VideoController {
-//
-//    @Value("${OPENVIDU.URL}")
-//    private String OPENVIDU_URL;
-//
-//    @Value("${OPENVIDU.PASSWORD}")
-//    private String OPENVIDU_PASSWORD;
-//
-//
-//}
+package mate.controller.video;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import mate.service.UserService;
+
+@RestController
+@RequestMapping("/video")
+@CrossOrigin(origins = {})
+public class VideoController {
+
+	@Value("${OPENVIDU.URL}")
+	private String OPENVIDU_URL;
+
+	@Value("${OPENVIDU.PASSWORD}")
+	private String OPENVIDU_PASSWORD;
+
+	private UserService userService;
+
+}

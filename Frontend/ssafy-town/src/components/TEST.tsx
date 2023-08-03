775a30a3dd75613310d73d5f3eec5466e6b227d1
이성민 <lrune2el@naver.com>
2023-08-03 15:46:55 +0900

    Test : websocket 테스트 코드 추가

---
+++ Frontend/ssafy-town/src/components/TEST.tsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import { Client, Message, Stomp } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+
+const MyComponent: React.FC = () => {
+  const [messageInput, setMessageInput] = useState('');
+  const [receivedMessages, setReceivedMessages] = useState<string[]>([]);
+  const stompClientRef = React.useRef<Client | null>(null);
+  const socket = new SockJS("http://localhost:8080/chatting");
+  
+  useEffect(() => { 
+    stompClientRef.current = Stomp.over(socket);
+
+    // 연결 시도
+    stompClientRef.current.activate();
+
+    return () => {
+      // 컴포넌트 언마운트 시 연결 해제
+      if (stompClientRef.current) {
+        stompClientRef.current.deactivate();
+      }
+    };
+  }, []);
+
+  const onMessageReceived = (message: Message) => {
+    const newMessage = message.body;
+    setReceivedMessages((prevMessages) => [...prevMessages, newMessage]);
+  };
+
+  useEffect(() => {
+    // 구독 설정
+    if (stompClientRef.current) {
+      stompClientRef.current.onConnect = (frame) => {
+        stompClientRef.current!.subscribe('/topic/1', onMessageReceived);
+      };
+    }
+  }, []);
+
+  const sendMessage = (message: string) => {
+    if (stompClientRef.current) {
+    const data = {
+        'sender': 'userId', // Set the sender's userId here
+        'contents': message,
+        };
+    stompClientRef.current.publish({
+        destination: '/app/chat/send',
+        body: JSON.stringify(data),
+        });
+      setMessageInput('');
+    }
+  };
+
+  return (
+    <div>
+      <h1>STOMP Client Example</h1>
+      <div>
+        <input
+          type="text"
+          value={messageInput}
+          onChange={(e) => setMessageInput(e.target.value)}
+        />
+        <button onClick={() => sendMessage(messageInput)}>Send</button>
+      </div>
+      <div>
+        <h2>Received Messages</h2>
+        <ul>
+          {receivedMessages.map((message, index) => (
+            <li key={index}>{message}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default MyComponent;
--- Frontend/ssafy-town/src/components/sidebar/5chat.tsx
+++ Frontend/ssafy-town/src/components/sidebar/5chat.tsx
@@ -1,8 +1,8 @@
-  // src/App.tsx
-  import React, { useState, useEffect } from "react";
-  import { io, Socket } from "socket.io-client";
-  import chat_css from "./5chat.module.css";
-
+// src/App.tsx
+import React, { useState, useEffect } from "react";
+import chat_css from "./5chat.module.css";
+import { Client, Message, Stomp } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
 import { useDispatch } from 'react-redux';
 import { setAllowMove } from '../../store/actions';
 
@@ -14,96 +14,42 @@ import { setAllowMove } from '../../store/actions';
   const Chat: React.FC = () => {
     const dispatch = useDispatch()
 
-    const [socket, setSocket] = useState<Socket | null>(null);
+    const stompClientRef = React.useRef<Client | null>(null);
+    const socket = new SockJS("http://localhost:8080/chatting");
+
     const [messages, setMessages] = useState<ChatMessage[]>([]);
     const [messageInput, setMessageInput] = useState<string>("");
     const [roomList, setRoomList] = useState<string[]>([]);
     const [selectedRoom, setSelectedRoom] = useState<string>("");
     const [oldestMessageTimestamp, setOldestMessageTimestamp] = useState<number | null>(null);
 
-    // input 방향키 살리기
-    const handlekeydown = (event:React.KeyboardEvent<HTMLInputElement>) => {
-      const inputElement = event.currentTarget
-      const currentCursorPosition = inputElement.selectionStart || 0;
-      if (event.key === 'ArrowLeft' && currentCursorPosition!==0) {
-        inputElement.setSelectionRange(currentCursorPosition - 1, currentCursorPosition - 1);
-      } else if (event.key === 'ArrowRight') {
-        inputElement.setSelectionRange(currentCursorPosition + 1, currentCursorPosition + 1);
-      } else if (event.key === ' '){
-        inputElement.value = inputElement.value.slice(0,currentCursorPosition)+ ' ' +inputElement.value.slice(currentCursorPosition,)
-        inputElement.setSelectionRange(currentCursorPosition+1 , currentCursorPosition+1);
-      }
+      // input 방향키 살리기
+  const handlekeydown = (event:React.KeyboardEvent<HTMLInputElement>) => {
+    const inputElement = event.currentTarget
+    const currentCursorPosition = inputElement.selectionStart || 0;
+    if (event.key === 'ArrowLeft' && currentCursorPosition!==0) {
+      inputElement.setSelectionRange(currentCursorPosition - 1, currentCursorPosition - 1);
+    } else if (event.key === 'ArrowRight') {
+      inputElement.setSelectionRange(currentCursorPosition + 1, currentCursorPosition + 1);
+    } else if (event.key === ' '){
+      inputElement.value = inputElement.value.slice(0,currentCursorPosition)+ ' ' +inputElement.value.slice(currentCursorPosition,)
+      inputElement.setSelectionRange(currentCursorPosition+1 , currentCursorPosition+1);
     }
-
+  }
+    // 연결에 관한 것
     useEffect(() => {
-      // Socket.IO 연결
-      const newSocket = io("http://i9b206.p.ssafy.io:9090"); // 백엔드 서버의 URL로 변경해주세요.
-      setSocket(newSocket);
-    
-      newSocket.on("connect", () => {
-        console.log("Connected to Socket.IO");
-      });
-    
-      // 채팅 메시지 수신
-      newSocket.on("chat", (message: ChatMessage) => {
-        setMessages((prevMessages) => [...prevMessages, message]);
-      });
-    
-      // 채팅방 목록 수신
-      newSocket.on("roomList", (rooms: string[]) => {
-        setRoomList(rooms);
-      });
-      setRoomList(['김싸피','이싸피']);
-    
+      stompClientRef.current = Stomp.over(socket);
+      // 연결 시도
+      stompClientRef.current.activate();
+
       return () => {
-        // 컴포넌트 언마운트 시 Socket.IO 연결 종료 및 이벤트 리스너 제거
-        newSocket.disconnect();
-        newSocket.off("chat"); // 채팅 이벤트 리스너 제거
+        // 컴포넌트 언마운트 시 연결 해제
+        if (stompClientRef.current) {
+          stompClientRef.current.deactivate();
+        }
       };
-    }, []);
+    }, [stompClientRef,socket]);
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setMessageInput(event.target.value);
-    };
-
-    const handleSendMessage = () => {
-      if (!selectedRoom) {
-        alert("채팅방을 선택해주세요.");
-        return;
-      }
-
-      // 서버로 메시지 전송
-      socket?.emit("sendMessage", {
-        content: messageInput,
-        sender: "User", // 사용자 이름 또는 식별자로 변경해주세요.
-        room: selectedRoom,
-      });
-      setMessageInput("");
-    };
-
-    const handleJoinRoom = (roomName: string) => {
-      setSelectedRoom(roomName);
-      socket?.emit("joinRoom", roomName);
-    
-      // 새로운 방에 들어가면 메시지와 oldestMessageTimestamp를 초기화합니다.
-      setMessages([]);
-      setOldestMessageTimestamp(null);
-    
-      // 선택한 채팅방에 대한 최신 메시지를 불러옵니다.
-      // 이 부분에서 백엔드로 API 호출을 하거나, Socket.IO를 통해 처리할 수 있습니다.
-      // 메시지를 업데이트하고, 가장 오래된 메시지의 타임스탬프를 저장합니다.
-    };
-
-    const handleScroll = (event: React.UIEvent<HTMLDivElement>) => {
-      const target = event.currentTarget;
-      if (target.scrollTop === 0 && oldestMessageTimestamp !== null) {
-        // 이전 메시지를 불러올 수 있도록 oldestMessageTimestamp를 이용합니다.
-        // 이 부분에서 백엔드로 API 호출을 하거나, Socket.IO를 통해 처리할 수 있습니다.
-        // 새롭게 불러온 메시지로 메시지 상태를 업데이트합니다.
-    
-        // 이전 메시지를 불러온 후, 가장 오래된 메시지의 타임스탬프를 업데이트합니다.
-      }
-    };
 
     return (
       <div className="sidebar_modal">
@@ -120,7 +66,7 @@ import { setAllowMove } from '../../store/actions';
           <div className={chat_css.scrollbox}>
             {roomList.map((room) => (
               <div>
-                <div className={chat_css.chat_room} key={room}  onClick={() => handleJoinRoom(room)}>
+                <div className={chat_css.chat_room} key={room}  onClick={() => {}}>
                   <img src="assets/default_profile.png" alt=""/>
                   <div className={chat_css.chat_roomdata}>
                     <div className={chat_css.roomdata} style={{marginBottom:'10px'}}>
@@ -139,28 +85,29 @@ import { setAllowMove } from '../../store/actions';
           </div>
         </div>
         :
-        <div style={{width:'80%'}}>
-          <div className={chat_css.chatstatus}>
-            <p onClick={() => {setSelectedRoom('')}}>back</p>
-            <h3>{selectedRoom}</h3>
-            <p>나가기</p>
-          </div>
-          <hr />
-          <div onScroll={handleScroll} style={{height:'80vh'}}>
-            {messages.map((message, index) => (
-              <div key={index}>
-                <strong>{message.sender}: </strong>
-                {message.content}
-              </div>
-            ))}
-          </div>
-          <hr />
-          <div>
-            <input type="text" value={messageInput} onChange={handleInputChange} style={{width:'80%'}} onKeyDown={handlekeydown} 
-            onFocus={()=>dispatch(setAllowMove(false))} onBlur={()=>dispatch(setAllowMove(true))}/>
-            <button onClick={handleSendMessage}>전송</button>
-          </div>
-        </div>
+        // <div style={{width:'80%'}}>
+        //   <div className={chat_css.chatstatus}>
+        //     <p onClick={() => {setSelectedRoom('')}}>back</p>
+        //     <h3>{selectedRoom}</h3>
+        //     <p>나가기</p>
+        //   </div>
+        //   <hr />
+        //   <div onScroll={handleScroll} style={{height:'80vh'}}>
+        //     {messages.map((message, index) => (
+        //       <div key={index}>
+        //         <strong>{message.sender}: </strong>
+        //         {message.content}
+        //       </div>
+        //     ))}
+        //   </div>
+        //   <hr />
+        //   <div>
+        //     <input type="text" value={messageInput} onChange={handleInputChange} style={{width:'80%'}} onKeyDown={handlekeydown} 
+        //     onFocus={()=>dispatch(setAllowMove(false))} onBlur={()=>dispatch(setAllowMove(true))}/>
+        //     <button onClick={handleSendMessage}>전송</button>
+        //   </div>
+        // </div>
+        null
         }
       </div>
     );

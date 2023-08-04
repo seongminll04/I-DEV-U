b2a9d575c37536367a751a423537d2e23c5216ba
박해종 <tgic01@korea.ac.kr>
2023-08-04 10:29:13 +0900

    Fix:카카오로그인

--- Frontend/ssafy-town/src/components/account/kakaologin.tsx
+++ Frontend/ssafy-town/src/components/account/kakaologin.tsx
@@ -1,50 +1,54 @@
 import { useEffect } from "react";
-import axios from "axios"
+import axios from "axios";
 import { useNavigate } from 'react-router-dom';
 
 const KakaoCallback = () => {
-    const navigate = useNavigate(); // React Router의 useNavigate 훅을 사용하여 페이지 이동 기능 추가
+    const navigate = useNavigate();
 
     useEffect(() => {
-        const params= new URL(document.location.toString()).searchParams;
-        const code = params.get('code');
-        const grantType = "authorization_code";
-        const REST_API_KEY = `${process.env.REACT_APP_REST_API_KEY}`;
-        const REDIRECT_URI = `${process.env.REACT_APP_REDIRECT_URL}`;
+        const fetchKakaoToken = async () => {
+            const params = new URL(document.location.toString()).searchParams;
+            const code = params.get('code');
+            const grantType = "authorization_code";
+            const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
+            const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URL;
 
-        axios.post(
-            `https://kauth.kakao.com/oauth/token?grant_type=${grantType}&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${code}`,
-            {},
-            { headers: { "Content-type": "application/x-www-form-urlencoded;charset=utf-8" } }
-        )
-        .then((res: any) => {
-            console.log(res);
-            const { access_token } = res.data;
-            axios.post(
-                `https://kapi.kakao.com/v2/user/me`,
-                {},
-                {
-                    headers: {
-                        Authorization: `Bearer ${access_token}`,
-                        "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+            try {
+                const responseToken = await axios.post(
+                    `https://kauth.kakao.com/oauth/token?grant_type=${grantType}&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${code}`,
+                    {},
+                    { headers: { "Content-type": "application/x-www-form-urlencoded;charset=utf-8" } }
+                );
+
+                const { access_token } = responseToken.data;
+
+                const res = await axios.post(
+                    `https://kapi.kakao.com/v2/user/me`,
+                    {},
+                    {
+                        headers: {
+                            Authorization: `Bearer ${access_token}`,
+                            "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+                        }
                     }
-                }
-            )
-            .then((res: any) => {
-                console.log('2번쨰', res);
+                );
+
+                console.log('User Info:', res.data);
                 localStorage.setItem('usertoken', res.data.access_token);
-                navigate('/home'); // 로그인 성공 후 /home으로 이동
-            })
-        })
-        .catch((Error: any) => {
-            console.log(Error)
-        })
-    }, [navigate]) // useEffect의 종속성 배열에 navigate를 추가
-    
+                navigate('/home');
+
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchKakaoToken();
+
+    }, [navigate]);
+
     return (
-        <div>
-        </div>
-    )
+        <div></div>
+    );
 }
 
 export default KakaoCallback;

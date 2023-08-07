60e5df68355bec0e239e4e7f0b99c1a7f33889f8
bmsnc <qjatjs98@gmail.com>
2023-08-07 23:48:20 +0900

    Feat : 파일 업로드

--- Backend/mate/src/main/java/mate/service/user/UserService.java
+++ Backend/mate/src/main/java/mate/service/user/UserService.java
@@ -193,7 +193,7 @@ public class UserService {
         File file = new File(filePath);
         multipartFile.transferTo(file);
 
-        user.uploadFile(fileName, filePath);
+        user.uploadFile(multipartFile.getOriginalFilename(), filePath);
         return Result.builder().status(ok().body("업로드 성공")).data(map).build();
     }
 

6ab7433801db3d6c564a0dd34a9804685b14042f
이진호 <blueland1001@naver.com>
2023-08-07 12:18:43 +0900

    Feat : QnA 수정

--- Backend/mate/src/main/java/mate/controller/QuestionController.java
+++ Backend/mate/src/main/java/mate/controller/QuestionController.java
@@ -2,6 +2,7 @@ package mate.controller;
 
 import lombok.RequiredArgsConstructor;
 import mate.domain.question.QuestionBoard;
+import mate.domain.question.QuestionBoardLike;
 import mate.service.QuestionService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,7 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/question")
+@RequestMapping("/qna")
 @RequiredArgsConstructor
 public class QuestionController {
 
@@ -21,6 +22,7 @@ public class QuestionController {
         Map<String, Object> map = new HashMap<>();
 
         questionService.writeQuestion(question);
+        map.put("resmsg", "Q&A 글 작성 성공");
 
         return ResponseEntity.ok(map);
     }
@@ -31,7 +33,8 @@ public class QuestionController {
 
         QuestionBoard question = questionService.detailQuestion(questionIdx);
 
-        map.put("notification", question);
+        map.put("resmsg", "Q&A 글 조회 성공");
+        map.put("Q&A", question);
 
         return ResponseEntity.ok(map);
     }
@@ -41,6 +44,7 @@ public class QuestionController {
         Map<String, Object> map = new HashMap<>();
 
         questionService.modifyQuestion(question);
+        map.put("resmsg", "Q&A 글 수정 성공");
 
         return ResponseEntity.ok(map);
     }
@@ -50,6 +54,7 @@ public class QuestionController {
         Map<String, Object> map = new HashMap<>();
 
         questionService.deleteQuestion(questionIdx);
+        map.put("resmsg", "Q&A 글 삭제 성공");
 
         return ResponseEntity.ok(map);
     }
@@ -59,7 +64,8 @@ public class QuestionController {
         System.out.println("questionAll");
         Map<String, Object> map = new HashMap<>();
 
-        map.put("list", questionService.getQuestionList());
+        map.put("resmsg", "Q&A 글 리스트 조회 성공");
+        map.put("Q&A", questionService.getQuestionList());
         return ResponseEntity.ok(map);
     }
 
@@ -67,7 +73,9 @@ public class QuestionController {
     public ResponseEntity<Map<String, Object>> getQuestionTop(@PathVariable("page") int page) {
         System.out.println("questionPage");
         Map<String, Object> map = new HashMap<>();
-        map.put("list", questionService.getQuestionList().subList(10 * (page - 1), 10 * page));
+
+        map.put("resmsg", "Q&A 글 리스트 조회 성공");
+        map.put("Q&A", questionService.getQuestionList().subList(10 * (page - 1), 10 * page));
         return ResponseEntity.ok(map);
     }
 
@@ -77,7 +85,8 @@ public class QuestionController {
         System.out.println("title = " + keyWord);
         Map<String, Object> map = new HashMap<>();
 
-        map.put("list", questionService.findQuestionByTitle(keyWord));
+        map.put("resmsg", "Q&A 글 리스트 조회 성공");
+        map.put("Q&A", questionService.findQuestionByTitle(keyWord));
         return ResponseEntity.ok(map);
     }
 
@@ -87,7 +96,29 @@ public class QuestionController {
         System.out.println("content = " + keyWord);
         Map<String, Object> map = new HashMap<>();
 
-        map.put("list", questionService.findQuestionByContent(keyWord));
+        map.put("resmsg", "Q&A 글 리스트 조회 성공");
+        map.put("Q&A", questionService.findQuestionByContent(keyWord));
+        return ResponseEntity.ok(map);
+    }
+
+    @GetMapping("/find/user/{name}")
+    public ResponseEntity<Map<String, Object>> findQuestionByName(
+            @PathVariable(value = "name", required = false) String name) {
+        System.out.println("name = " + name);
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("resmsg", "Q&A 글 리스트 조회 성공");
+        map.put("Q&A", questionService.findQuestionByName(name));
+        return ResponseEntity.ok(map);
+    }
+
+    @PostMapping("/like")
+    public ResponseEntity<Map<String, Object>> like(QuestionBoardLike like) {
+        Map<String, Object> map = new HashMap<>();
+
+        questionService.writeQuestionLike(like);
+        map.put("resmsg", "Q&A 글 좋아요 성공");
+
         return ResponseEntity.ok(map);
     }
 }
--- Backend/mate/src/main/java/mate/service/QuestionService.java
+++ Backend/mate/src/main/java/mate/service/QuestionService.java
@@ -2,6 +2,8 @@ package mate.service;
 
 import lombok.RequiredArgsConstructor;
 import mate.domain.question.QuestionBoard;
+import mate.domain.question.QuestionBoardLike;
+import mate.repository.QuestionLikeRepository;
 import mate.repository.QuestionRepository;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,7 @@ import java.util.Optional;
 public class QuestionService {
 
     private final QuestionRepository questionRepository;
+    private final QuestionLikeRepository questionLikeRepository;
 
     public void writeQuestion(QuestionBoard questionBoard) {
         questionRepository.save(questionBoard);
@@ -47,5 +50,13 @@ public class QuestionService {
 
     public List<QuestionBoard> findQuestionByContent(String keyWord) {
         return questionRepository.findByContentContainingOrderByIdxDesc(keyWord);
+    }
+
+    public List<QuestionBoard> findQuestionByName(String name) {
+        return questionRepository.findByNameOrderByIdxDesc(name);
+    }
+
+    public void writeQuestionLike(QuestionBoardLike like){
+        questionLikeRepository.save(like);
     }
 }

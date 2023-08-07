a039ba906cb3e6b725dfe5ebf3a961ff6813d5d1
bmsnc <qjatjs98@gmail.com>
2023-08-07 17:16:06 +0900

    Feat : 파일 업로드

--- Backend/mate/src/main/java/mate/global/config/WebMvcConfiguration.java
+++ Backend/mate/src/main/java/mate/global/config/WebMvcConfiguration.java
@@ -2,12 +2,10 @@ package mate.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
-@Order(1)
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
@@ -17,11 +15,10 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/image/**").addResourceLocations("file:///" + "/mate-server/upload/img/user/")
+        registry.addResourceHandler("/image/**").addResourceLocations("file:///" + uploadDir)
                 .setCachePeriod(3600)
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver());
                                     //mate-server/upload/images/user/17_꼬부기.png
     }
-
 }

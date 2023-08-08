b602e5ba45d212ff94fff017025d539167e61dd8
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2023-08-08 22:40:22 +0900

    Feat : PartnerController 수정

--- Backend/mate/src/main/java/mate/dto/partner/DetailDto.java
+++ Backend/mate/src/main/java/mate/dto/partner/DetailDto.java
@@ -2,8 +2,13 @@ package mate.dto.partner;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class DetailDto {
 	private int userIdx;
-	
+	private String name;
+	private String nickname;
+	private String intro;
+	private List<String> techList;
 }
--- Backend/mate/src/main/java/mate/service/partner/PartnerService.java
+++ Backend/mate/src/main/java/mate/service/partner/PartnerService.java
@@ -3,6 +3,8 @@ package mate.service.partner;
 import java.util.ArrayList;
 import java.util.List;
 
+import mate.domain.user.User;
+import mate.dto.partner.DetailDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,15 +22,15 @@ public class PartnerService {
 	private final PartnerRepository partnerRepository;
 	private final UserRepository userRepository;
 
-	public List<PartnerDto> listPartner(List<TagDto> input) {
+	public List<PartnerDto> listPartner(List<String> input) {
 
-		List<String> tagList = new ArrayList<>();
+//		List<String> tagList = new ArrayList<>();
+//
+//		for (TagDto tagDto : input) {
+//			tagList.add(tagDto.getTag());
+//		}
 
-		for (TagDto tagDto : input) {
-			tagList.add(tagDto.getTag());
-		}
-
-		List<Object> partners = partnerRepository.listPartner(tagList, tagList.size());
+		List<Object> partners = partnerRepository.listPartner(input, input.size());
 
 		List<PartnerDto> list = new ArrayList<>();
 
@@ -51,27 +53,28 @@ public class PartnerService {
 		return list;
 	}
 
-	public Object detailPartner(int userIdx) {
-		Object detailPartner = partnerRepository.detailPartner(userIdx);
+	public DetailDto detailPartner(int userIdx) {
+		List<Object> techs = partnerRepository.findTech(userIdx);
 
-		// List<UserDto> list = new ArrayList<>();
-		//
-		// for (Object o : detailPartner) {
-		// 	Object[] result = (Object[])o;
-		//
-		// 	// Assuming the order of elements in the array corresponds to the order of fields in PartnerDto
-		// 	Long userIdx
-		// 	String name = (String)result[0];
-		// 	String nickname = (String)result[1];
-		// 	Long percent = (Long)result[2];
-		//
-		// 	PartnerDto partnerDto = new PartnerDto();
-		// 	partnerDto.setName(name);
-		// 	partnerDto.setNickname(nickname);
-		// 	partnerDto.setPercent(percent);
-		//
-		// 	list.add(partnerDto);
-		// }
+		List<String> techList = new ArrayList<>();
+
+		for (Object o : techs) {
+			Object[] result = (Object[])o;
+
+			String tech = (String)result[1];
+
+			techList.add(tech);
+		}
+
+		User user = userRepository.findById(userIdx).get();
+
+		DetailDto detailPartner = new DetailDto();
+
+		detailPartner.setUserIdx(user.getIdx());
+		detailPartner.setName(user.getName());
+		detailPartner.setNickname(user.getNickname());
+		detailPartner.setIntro(user.getIntro());
+		detailPartner.setTechList(techList);
 
 		return detailPartner;
 	}

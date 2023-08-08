8a48a1113f8ea38e3391b75ab3da3aa90c21d670
이성민 <lrune2el@naver.com>
2023-08-08 23:48:13 +0900

    Feat : websocket sub 컴포넌트 세분화

--- Frontend/ssafy-town/src/components/sidebar/5chatroom.tsx
+++ Frontend/ssafy-town/src/components/sidebar/5chatroom.tsx
@@ -1,24 +1,107 @@
 import React, { useState,useEffect } from 'react';
 import chat_css from "./5chat.module.css";
 
-import { Client } from '@stomp/stompjs';
-
+import { Client, Message } from '@stomp/stompjs';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { setAllowMove, setReceiveMessages, setSidebar } from '../../store/actions';
+import { setAllowMove, setSidebar } from '../../store/actions';
 import { AppState } from '../../store/state';
 import axios from 'axios';
+
+interface messageProps {
+  userIdx:number,
+  userName:string,
+  messageIdx:number,
+  message:string,
+  created_at:string,
+}
 
 const Chatroom: React.FC = () => {
   const dispatch = useDispatch()
   const [messageInput, setMessageInput] = useState('');
-  const receivedMessages = useSelector((state: AppState) => state.receivedMessages);
+
   const stompClientRef = React.useRef<Client | null>(null);
   stompClientRef.current = useSelector((state: AppState) => state.stompClientRef)
+
+  const [receivedMessages, setReceiveMessages] = useState<messageProps[]>([])
+  const chatScrollRef = React.useRef<HTMLDivElement | null>(null); // Ref for chat_scroll div
+
+  const isChatIdx = useSelector((state: AppState) => state.isChatIdx);
   const userIdx = localStorage.getItem('userIdx')
-  useEffect(()=>{
-    dispatch(setReceiveMessages([]))
-  },[dispatch])
+
+  const userToken = localStorage.getItem('userToken')
+  axios({
+    method:'get',
+    url:'https://i9b206.p.ssafy.io:9090/chat/load',
+    data:{
+      chatIdx : isChatIdx,
+      nowFirstIdx : null,
+    },
+    headers : {
+      Authorization: 'Bearer ' + userToken
+    }})
+  .then(res=>{
+    setReceiveMessages([res.data,...receivedMessages])
+    if (chatScrollRef.current && chatScrollRef.current.scrollHeight > chatScrollRef.current.clientHeight) {
+      // Scroll chat_scroll to the bottom initially
+      chatScrollRef.current.scrollTop = chatScrollRef.current.scrollHeight;
+    }
+  })
+  .catch(err=>console.log(err))
+
+  // 구독등록
+  useEffect(() => {
+    if (stompClientRef.current) {
+      stompClientRef.current.subscribe(`/sub/chatRoom/1`, function(message: Message) {
+        // const newMessage = message.body;
+        if (chatScrollRef.current && chatScrollRef.current.scrollHeight > chatScrollRef.current.clientHeight && chatScrollRef.current.scrollTop === chatScrollRef.current.scrollHeight) {
+          setReceiveMessages([...receivedMessages, {
+            'userIdx':1,
+            'userName':'김싸피',
+            'messageIdx':1,
+            'message':'내용',
+            'created_at':'보낸시간',
+          }])
+          chatScrollRef.current.scrollTop = chatScrollRef.current.scrollHeight
+        }
+        else {
+          setReceiveMessages([...receivedMessages, {
+            'userIdx':1,
+            'userName':'김싸피',
+            'messageIdx':1,
+            'message':'내용',
+            'created_at':'보낸시간',
+          }])
+        }
+      })
+      }
+
+      return () => {
+        if (stompClientRef.current) {
+        stompClientRef.current.unsubscribe(`/sub/chatRoom/1`)}
+    }
+  }, [stompClientRef,receivedMessages]);
+
+
+  // 끝까지 스크롤 시 추가로딩
+  const addload = () => {
+    const userToken = localStorage.getItem('userToken')
+    axios({
+      method:'get',
+      url:'https://i9b206.p.ssafy.io:9090/chat/load',
+      data:{
+        chatIdx:isChatIdx,
+        nowFirstIdx : receivedMessages[0].messageIdx
+      },
+      headers : {
+        Authorization: 'Bearer ' + userToken
+      }})
+    .then(res=>{
+      setReceiveMessages([res.data,...receivedMessages])
+    })
+    .catch(err=>console.log(err))
+  }
+
   const handlekeydown = (event:React.KeyboardEvent<HTMLInputElement>) => {
     const inputElement = event.currentTarget
     const currentCursorPosition = inputElement.selectionStart || 0;
@@ -31,13 +114,7 @@ const Chatroom: React.FC = () => {
       inputElement.setSelectionRange(currentCursorPosition+1 , currentCursorPosition+1);
     }
   }
-  useEffect(()=>{
-    axios({
-      method:'get',
-      url:'',
-
-    })
-  })
+  
   const sendMessage = (message: string) => {
     if (stompClientRef.current) {
     const data = {
@@ -54,14 +131,12 @@ const Chatroom: React.FC = () => {
 
   const handleScroll = (event: React.UIEvent<HTMLDivElement>) => {
     const target = event.currentTarget;
-    if (target.scrollTop === 0) {
-    // 원본 if (target.scrollTop === 0 && oldestMessageTimestamp !== null) {
-      // 이전 메시지를 불러올 수 있도록 oldestMessageTimestamp를 이용합니다.
-      // 이 부분에서 백엔드로 API 호출을 하거나, Socket.IO를 통해 처리할 수 있습니다.
-      // 새롭게 불러온 메시지로 메시지 상태를 업데이트합니다.
-      // 이전 메시지를 불러온 후, 가장 오래된 메시지의 타임스탬프를 업데이트합니다.
+    if (target.scrollHeight > target.clientHeight && target.scrollTop === 0) {
+      // Load more messages when scrolling to the top
+      addload();
     }
   };
+
 
   return (
     <div className="sidebar_modal">
@@ -72,14 +147,16 @@ const Chatroom: React.FC = () => {
         <p onClick={()=>{}}>나가기</p>
       </div>
       <hr />
-      <div onScroll={handleScroll} style={{height:'80vh'}}>
+      </div>
+      <div className={chat_css.chat_scroll} onScroll={handleScroll} style={{height:'80vh'}} ref={chatScrollRef}>
         {receivedMessages.map((message, index) => (
-          <div key={index}>
-            <strong>{message} 보낸 사람 : </strong>
-            {/* {message} 보낸내용 */}
+          <div key={index} style={{width:'80%', margin:'auto'}} >
+            <strong>{message.userName} : </strong>
+            {message.message}    {message.created_at}
           </div>
         ))}
       </div>
+      <div style={{width:'80%'}}>
       <hr />
       <div>
         <input type="text" value={messageInput} onChange={(e)=>setMessageInput(e.target.value)} style={{width:'80%'}} onKeyDown={handlekeydown} 

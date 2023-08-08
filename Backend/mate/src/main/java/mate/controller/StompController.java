1def294d01d77c23b4e567fe42331bbb83f8ee67
bmsnc <qjatjs98@gmail.com>
2023-08-08 14:56:21 +0900

    Feat : WebSocket 설정 완료

--- Backend/mate/src/main/java/mate/controller/StompController.java
+++ Backend/mate/src/main/java/mate/controller/StompController.java
@@ -15,7 +15,7 @@ public class StompController {
 
     @MessageMapping("/chat/send")
     public void sendMsg(@Payload Map<String,Object> data){
-        simpMessagingTemplate.convertAndSend("/sub/1",data);
+        simpMessagingTemplate.convertAndSend("/sub/room/1",data);
     }
 
 }

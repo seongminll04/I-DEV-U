925873a373fa51b6d50f5ef042d6b0a6e7d7fbb3
이형석 <gudtjr2949@naver.com>
2023-08-11 13:34:39 +0900

    Feat : openvidu index.js 수정

--- Frontend/openvidu-basic-node/index.js
+++ Frontend/openvidu-basic-node/index.js
@@ -1,11 +1,12 @@
 require("dotenv").config(!!process.env.CONFIG ? {path: process.env.CONFIG} : {});
 var express = require("express");
 var bodyParser = require("body-parser");
-var http = require("http");
+var https = require("https");
 var OpenVidu = require("openvidu-node-client").OpenVidu;
 var socketIo = require('socket.io');
 var cors = require("cors");
 var app = express();
+const fs = require("fs");
 
 // 노드서버
 var SERVER_PORT = process.env.SERVER_PORT;
@@ -14,20 +15,38 @@ var OPENVIDU_URL = process.env.OPENVIDU_URL;
 // opvidu 서버 비번
 var OPENVIDU_SECRET = process.env.OPENVIDU_SECRET;
 
+const privateKey = fs.readFileSync("privkey.pem", "utf8");
+const certificate = fs.readFileSync("cert.pem", "utf8")
+const ca = fs.readFileSync("fullchain.pem", "utf8")
+
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+};
+
 // Cors 지금은 전체인데 나중에 바꿔줘야함
 app.use(
   cors({
-    origin: "*",
+    // origin: "*",
+        origin: "https://i9b206.p.ssafy.io",
   })
 );
 
+
 // express 로 HTTP 서버를 생성
-var server = http.createServer(app);
+var server = https.createServer(credentials, app);
 // openvidu 클라이언트 객체를 초기화
 var openvidu = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
 openvidu.activeSessions = [];
 // socket.io 초기화
-var io = socketIo(server);
+const io = socketIo(server, {
+    cors: {
+    origin: "https://i9b206.p.ssafy.io",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  },
+});
 
 // express 앱에 bodyParser 미들웨어를 추가하여 POST 요청의 body를 쉽게 파싱
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -92,27 +111,55 @@ var userSessions = {};
 
 io.on('connection', function(socket) {
     socket.on('offer', function(data){
-        socket.to(data.target).emit('offer', data);
+        const targetSocketId = Object.keys(userSessions).find(key => userSessions[key].userId === data.target);
+        if (targetSocketId) {
+            socket.to(targetSocketId).emit('offer', data);
+        }
+        console.log("22222222222222222222222222222222222")
+        console.log(data.target,"offer")
     });
 
-    socket.on('answer', function(data){
-        socket.to(data.target).emit('answer', data);
+    socket.on('answer', function(data) {
+        const targetSocketId = Object.keys(userSessions).find(key => userSessions[key].userId === data.target);
+        if (targetSocketId) {
+            socket.to(targetSocketId).emit('answer', data);
+        }
+        console.log("333333333333333333333333333333333333333");
+        console.log(data.target,"answer")
     });
 
-    socket.on('ice-candidate', function(data){
-        socket.to(data.target).emit('ice-candidate', data);
+    socket.on('ice-candidate', function(data) {
+        const targetSocketId = Object.keys(userSessions).find(key => userSessions[key].userId === data.target);
+        if (targetSocketId) {
+            socket.to(targetSocketId).emit('ice-candidate', data);
+        }
+        console.log("444444444444444444444444444444444444");
+        console.log(data.target, "ice");
     });
 
     socket.on('join-session', function(data) {
-      
-      // 유저의 세션 정보를 저장
-      userSessions[socket.id] = {
-          userId: data.userId,
-          sessionId: data.sessionId
-      };
-      
-      // socket.io의 방을 사용하여 세션별로 유저들을 그룹화
-      socket.join(data.sessionId);
+        // 1. 현재 세션에 참여하고 있는 다른 사용자들의 ID를 가져옵니다.
+        const otherUserIdsInSession = Object.values(userSessions)
+        .filter(session => session.sessionId === data.sessionId && session.userId !== data.userId)
+        .map(session => session.userId);
+
+        // 2. 새로운 사용자에게 현재 세션에 참여하고 있는 다른 모든 사용자들의 ID를 전송합니다.
+        socket.emit('current-users', { userIds: otherUserIdsInSession });
+
+        // 3. 다른 사용자들에게 새로운 사용자가 참여했다는 것을 알립니다.
+        socket.to(data.sessionId).emit('new-peer', { userId: data.userId });
+
+        // 유저의 세션 정보를 저장
+        userSessions[socket.id] = {
+        userId: data.userId,
+        sessionId: data.sessionId
+        };
+
+        console.log(userSessions[socket.id], "@@@@@@@@@@@@@@@@@@@@@@"); // 현재 소켓의 세션 정보 출력
+        console.log(userSessions, "###### Entire userSessions object ######"); // 전체 userSessions 객체 출력
+
+        // socket.io의 방을 사용하여 세션별로 유저들을 그룹화
+        socket.join(data.sessionId);
     });
 
     socket.on('disconnect-user', function() {

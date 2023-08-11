99f33fd028f5a2aefb9e32fad90140cfe703b10f
bmsn <qjatjs98@naver.com>
2023-08-11 14:02:29 +0900

    Feat : 알림

--- Backend/mate/src/main/java/mate/alarm/service/AlarmService.java
+++ Backend/mate/src/main/java/mate/alarm/service/AlarmService.java
@@ -4,11 +4,17 @@ import lombok.RequiredArgsConstructor;
 import mate.alarm.domain.Alarm;
 import mate.alarm.domain.AlarmRepository;
 import mate.alarm.dto.AlarmRequest;
+import mate.alarm.dto.AlarmResponse;
+import mate.controller.Result;
 import mate.domain.user.User;
 import mate.global.exception.NotFoundException;
 import mate.repository.user.UserRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -29,7 +35,37 @@ public class AlarmService {
                 .orElseThrow(() -> new NotFoundException(NotFoundException.USER_NOT_FOUND));
 
         Alarm alarm = Alarm.createAlarm(request);
-        alarmRepository.save(alarm);
+        alarmRepository.saveAndFlush(alarm);
         alarmListener.sendAlarm(fromUser, toUser, alarm);
+    }
+
+    public Result findByFrom(Integer userIdx) {
+        Optional<List<Alarm>> findAlarm = alarmRepository.findFrom(userIdx);
+
+        if (findAlarm.isEmpty()){
+            AlarmResponse alarmResponse = new AlarmResponse();
+            return Result.builder().data(alarmResponse).status(ResponseEntity.ok("받은 알림 없음")).build();
+        }else{
+            AlarmResponse alarmResponse = new AlarmResponse();
+            return Result.builder().build();
+        }
+    }
+
+    public Result findByTo(Integer userIdx) {
+        Optional<List<Alarm>> findAlarm = alarmRepository.findTo(userIdx);
+
+        if (findAlarm.isEmpty()){
+            AlarmResponse alarmResponse = new AlarmResponse();
+            return Result.builder().data(alarmResponse).status(ResponseEntity.ok("보낸 알림 없음")).build();
+        }else{
+            AlarmResponse alarmResponse = new AlarmResponse();
+            return Result.builder().build();
+        }
+    }
+
+    @Transactional
+    public Result deleteByIdx(Integer idx) {
+        alarmRepository.deleteByIdx(idx);
+        return Result.builder().status(ResponseEntity.ok(idx + "번 알림 삭제 성공")).build();
     }
 }

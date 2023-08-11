ad9e1196bf0a4636c47158793fc75f9bb7478e6d
bmsn <qjatjs98@naver.com>
2023-08-11 10:25:43 +0900

    Feat : 알림

---
+++ Backend/mate/src/main/java/mate/alarm/domain/Alarm.java
@@ -0,0 +1,47 @@
+package mate.alarm.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import mate.alarm.dto.AlarmRequest;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Alarm {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idx;
+
+    private Integer fromIdx;
+    private Integer toIdx;
+
+    @Enumerated(EnumType.STRING)
+    private AlarmType type;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime createdAt;
+
+
+    public static Alarm createAlarm(AlarmRequest alarmRequest){
+        Alarm alarm = new Alarm();
+        alarm.fromIdx = alarmRequest.getFromIdx();
+        alarm.toIdx = alarmRequest.getToIdx();
+        alarm.type = alarmRequest.getType();
+        alarm.createdAt = alarmRequest.getCreatedAt();
+
+        return alarm;
+    }
+
+}
---
+++ Backend/mate/src/main/java/mate/alarm/dto/AlarmRequest.java
@@ -0,0 +1,19 @@
+package mate.alarm.dto;
+
+import lombok.Data;
+import mate.alarm.domain.AlarmType;
+
+import java.time.LocalDateTime;
+
+@Data
+public class AlarmRequest{
+
+    private Integer fromIdx;
+    private Integer toIdx;
+
+    private AlarmType type;
+
+    private LocalDateTime createdAt;
+
+
+}

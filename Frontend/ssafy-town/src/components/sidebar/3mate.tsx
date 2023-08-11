5011ae5d811b0fcb278987faabd20e1af07b8e3a
이진호 <blueland1001@naver.com>
2023-08-11 15:37:33 +0900

    Feat : 동료찾기 필터적용

--- Frontend/ssafy-town/src/components/sidebar/3mate.tsx
+++ Frontend/ssafy-town/src/components/sidebar/3mate.tsx
@@ -10,11 +10,11 @@ import MateFilter from '../filter/mateFilter';
 import MateDetail from '../detail/matedetail';
 
 interface Matep {
-  userIdx:number;
+  userIdx: number;
   name: string;
   nickname: string;
   percent: number;
-  languageList: Array<string>;
+  languageList: string[];
 }
 
 interface Filter {
@@ -26,29 +26,38 @@ interface Filter {
 const Mate: React.FC = () => {
   const dispatch = useDispatch();
   const isModalOpen = useSelector((state: AppState) => state.isModalOpen);//사이드바 오픈여부
-  const [matefilter,setMateFilter] = useState<Filter[]>([])
-  const [mateIdx,setMateIdx] = useState<number>(0)
+  const [matefilter, setMateFilter] = useState<Filter[]>([])
+  const [mateIdx, setMateIdx] = useState<number>(0)
   const [mateList, setMateList] = useState<Matep[]>([
-    {userIdx:0, "name": "김싸피", "nickname": "김김김", "percent": 55, "languageList": [] },
-    {userIdx:1, "name": "이싸피", "nickname": "이이이", "percent": 46, "languageList": [] },
-    {userIdx:2, "name": "박싸피", "nickname": "박박박", "percent": 37, "languageList": [] },
-    {userIdx:3, "name": "최싸피", "nickname": "최최최", "percent": 28, "languageList": [] },
+    { userIdx: 0, "name": "김싸피", "nickname": "김김김", "percent": 55, "languageList": [] },
+    { userIdx: 1, "name": "이싸피", "nickname": "이이이", "percent": 46, "languageList": [] },
+    { userIdx: 2, "name": "박싸피", "nickname": "박박박", "percent": 37, "languageList": [] },
+    { userIdx: 3, "name": "최싸피", "nickname": "최최최", "percent": 28, "languageList": [] },
   ]);
+
   useEffect(() => {
     const userToken = localStorage.getItem('userToken');
+    var tList: string[] = []
+    for (const filter of matefilter) {
+      tList = [...tList, ...filter.tagList]
+    }
+    console.log(tList);
     axios({
-      method: 'get',
+      method: 'post',
       url: 'https://i9b206.p.ssafy.io:9090/partner/list',
       headers: {
         Authorization: 'Bearer ' + userToken
       },
-      data : matefilter,
+      data: {
+        tagList: tList
+      },
     })
       .then(res => {
+        console.log(res);
         setMateList(res.data.userList);
       })
       .catch(err => console.log(err))
-  },[matefilter])
+  }, [matefilter])
 
   return (
     <div>
@@ -62,14 +71,14 @@ const Mate: React.FC = () => {
         <div className={mate_css.scrollbar}>
           {mateList.map((mate: Matep, index: number) => {
             return (
-              <div className={mate_css.usertable} onClick={()=>{setMateIdx(mate.userIdx); dispatch(setModal('동료상세정보'))}}>
+              <div className={mate_css.usertable} onClick={() => { setMateIdx(mate.userIdx); dispatch(setModal('동료상세정보')) }}>
                 <div className={mate_css.userInfo}>
                   <div className={mate_css.profile}>
                     <img src="assets/default_profile.png" alt="" />
                     <div className={mate_css.profiledata}>
                       <b>{mate.nickname}</b>
                       {mate.languageList.map((lang: string) => {
-                        return (<p style={{ color: 'gray', marginTop : 0, marginBottom: 0 }}>
+                        return (<p style={{ color: 'gray', marginTop: 0, marginBottom: 0 }}>
                           # {lang}
                         </p>)
                       })}
@@ -83,9 +92,9 @@ const Mate: React.FC = () => {
         </div>
         <p>-더 없음-</p>
       </div>
-      { isModalOpen === '동료찾기필터' ? <MateFilter filter={matefilter} onfilter={(value:Filter[])=>setMateFilter(value)} />
-      : isModalOpen === '동료상세정보' ? <MateDetail userIdx={mateIdx}/> 
-      : null}
+      {isModalOpen === '동료찾기필터' ? <MateFilter filter={matefilter} onfilter={(value: Filter[]) => setMateFilter(value)} />
+        : isModalOpen === '동료상세정보' ? <MateDetail userIdx={mateIdx} />
+          : null}
     </div>
   );
 };

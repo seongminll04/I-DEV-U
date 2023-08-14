e2378a22a8f1ff36f78b0fd5f3205f025e45eb1b
bmsnc <qjatjs98@gmail.com>
2023-08-15 00:19:05 +0900

    Feat : 유저 수정

--- Backend/mate/src/main/java/mate/service/user/UserService.java
+++ Backend/mate/src/main/java/mate/service/user/UserService.java
@@ -96,8 +96,8 @@ public class UserService {
                     .intro(Optional.ofNullable(userUpdateDto.getIntro()).orElse(user.getIntro()))
                     .role(user.getRole())
                     .status(user.getStatus())
-                    .originalFileName(userUpdateDto.getOriginalFileName())
-                    .storedFileName(userUpdateDto.getStoredFileName())
+                    .originalFileName(user.getOriginalFileName())
+                    .storedFileName(user.getStoredFileName())
                     .invite(user.getInvite())
                     .basicAnswerList(user.getBasicAnswerList())
                     .build();

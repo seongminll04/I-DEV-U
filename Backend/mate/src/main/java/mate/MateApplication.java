9933ac9997ae543c599e17b2fef076bc83c80301
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2023-08-15 19:31:53 +0900

    Feat : TimeZone 변경

--- Backend/mate/src/main/java/mate/MateApplication.java
+++ Backend/mate/src/main/java/mate/MateApplication.java
@@ -3,8 +3,16 @@ package mate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class MateApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MateApplication.class, args);
